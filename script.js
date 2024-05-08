640e9a803d6740a0de6d7a53ad1e983159e13a19
adi-ty-a <adi.personal.13@gmail.com>
2024-05-08 22:49:24 +0530

    button removes old and new is made

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
+let noofblock=16;
 const grid = document.querySelector("#container");
+//button 
+const button = document.querySelector("#noofsquare");
+button.addEventListener("click", () => {
+    noofblock = prompt("enter the no of block per side");
+    removediv();
+    creatediv();
+});
+
+//remove 
+function removediv(){
+let main = document.querySelectorAll(".subdiv")
+main.forEach((box) => {
+    box.remove();
+});
+};
 
 // 16*16 div to container;
-for(let i=0;i<=16;i++){
+function creatediv(){
+for(let i=0;i<=noofblock;i++){
     let subdiv = document.createElement("div");
     subdiv.classList.add("subdiv");
-    
-    for(let j=0 ;j<=16;j++){
+    for(let j=0 ;j<=noofblock;j++){
         let div= document.createElement("div");
         div.classList.add("box");
         subdiv.appendChild(div);
     }
-    
     grid.appendChild(subdiv);
 }
+//red color
+const newdiv = document.querySelectorAll(".box");
+newdiv.forEach((button) => {
+button.addEventListener("mouseover", () => {
+  button.style["background-color"] = "red";
+});
+});
+};
+// 1 cakk for grid 
+creatediv();
+
