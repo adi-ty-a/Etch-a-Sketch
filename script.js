4e81e748acc3c41bf3bd29520405d1747ddbabf6
adi-ty-a <adi.personal.13@gmail.com>
2024-05-08 16:38:19 +0530

    box style fixed

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const grid = document.querySelector("#container");
 
+// 16*16 div to container;
 for(let i=0;i<=16;i++){
     let subdiv = document.createElement("div");
-    subdiv.classList.add(i);
+    subdiv.classList.add("subdiv");
     
     for(let j=0 ;j<=16;j++){
         let div= document.createElement("div");
